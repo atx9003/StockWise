b492d92fda9b5aaf671e9d175f4e8575eaa0846a
PedroHenriqueMS-18 <petermenezes.2006@gmail.com>
2024-10-01 14:54:05 -0300

    Janela Modal

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+const openModalButton = document.querySelector("#open-modal-edit");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+// função
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
