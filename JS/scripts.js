c92a3b68d031cd3d5e3afe4d7ce20ba3bbab51aa
PedroHenriqueMS-18 <petermenezes.2006@gmail.com>
2024-10-03 13:41:10 -0300

    Novo Modal

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,14 +1,37 @@
-const openModalButton = document.querySelector("#open-modal-edit");
-const closeModalButton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
+// const openModalButton = document.querySelector("#open-modal-edit");
+// const closeModalButton = document.querySelector("#close-modal");
+// const modal = document.querySelector("#modal");
+// const fade = document.querySelector("#fade");
 
-// função
-const toggleModal = () => {
-  modal.classList.toggle("hide");
-  fade.classList.toggle("hide");
-};
+// // função
+// const toggleModal = () => {
+//   modal.classList.toggle("hide");
+//   fade.classList.toggle("hide");
+// };
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
-});
+// [openModalButton, closeModalButton, fade].forEach((el) => {
+//   el.addEventListener("click", () => toggleModal());
+// });
+
+function openModal(title) {
+  const modal = document.getElementById("modal");
+  const modalBody = document.querySelector(".modal-body");
+  const closeBtn = document.querySelector(".close");
+
+  modalBody.innerHTML = `
+     <h2>${title}</h2>
+  `;
+
+  modal.style.display = "block";
+
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // fechar modal ao clicar fora dele
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+}
