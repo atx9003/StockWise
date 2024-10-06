b1498149d56dbeb0342d0ee7d714e5f82b58f20d
PedroHenriqueMS-18 <petermenezes.2006@gmail.com>
2024-10-06 20:24:58 -0300

    Modal Create

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,18 +1,3 @@
-// const openModalButton = document.querySelector("#open-modal-edit");
-// const closeModalButton = document.querySelector("#close-modal");
-// const modal = document.querySelector("#modal");
-// const fade = document.querySelector("#fade");
-
-// // função
-// const toggleModal = () => {
-//   modal.classList.toggle("hide");
-//   fade.classList.toggle("hide");
-// };
-
-// [openModalButton, closeModalButton, fade].forEach((el) => {
-//   el.addEventListener("click", () => toggleModal());
-// });
-
 function openModal(title) {
   // obtem os elementos do HTML e armazena-os em uma variável
   const modal = document.getElementById("modal");
@@ -68,6 +53,70 @@ function openModal(title) {
   window.onclick = function (event) {
     if (event.target === modal) {
       modal.style.display = "none";
+    }
+  };
+}
+
+// Função para abrir o modal de adicionar um novo item
+function openModalCreate() {
+  const modalCreate = document.getElementById("modal-create");
+  const modalBodyCreate = document.querySelector(".modal-body-create");
+  const closeCreate = document.querySelector(".close-create");
+
+  modalCreate.style.display = "block";
+
+  modalBodyCreate.innerHTML = `
+    <div class="header">
+              <h2>Criar novo item</h2>
+            </div>
+            <form action="" class="form-edit">
+              <div class="box-input">
+                <label for="nome">Nome</label>
+                <input
+                  type="text"
+                  name="nome"
+                  placeholder="Insira o nome do produto"
+                  class="block"
+                />
+              </div>
+              <div class="box-input">
+                <label for="nome">Imagem</label>
+                <input
+                  type="file"
+                  name="nome"
+                  placeholder="Insira a imagem do produto"
+                  class="image"
+                />
+              </div>
+              <div class="box-input">
+                <label for="preco">Preço</label>
+                <input
+                  type="text"
+                  name=""
+                  placeholder="Insira o preço do produto"
+                  class="block"
+                />
+              </div>
+              <div class="box-input">
+                <label for="qtd">Quantidade</label>
+                <input
+                  type="text"
+                  name="qtd"
+                  placeholder="Insira o quantidade do produto"
+                  class="block"
+                />
+              </div>
+              <input type="submit" name="" id="salvar" value="Salvar" />
+            </form>
+  `;
+
+  closeCreate.onclick = function () {
+    modalCreate.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modalCreate) {
+      modalCreate.style.display = "none";
     }
   };
 }
